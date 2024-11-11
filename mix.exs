6f73f9175bdceaef75ebb4a38b56c6e36024a0d1
Ben Reinhart <benjreinhart@gmail.com>
2024-11-11 11:37:39 -0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ServerSentEvents.MixProject do
   def project do
     [
       app: :server_sent_events,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       name: "Server Sent Events",
