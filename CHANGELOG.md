8743715cc1bd5181f14ae14135ed81edd4c890b8
Ben Reinhart <benjreinhart@gmail.com>
2024-11-11 12:25:02 -0800

    Changelog and docs

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+## v0.2.0 (2024-11-11)
+
+- Use symbols for map keys in `parse/1`'s return value
+- Small refactorings
+- Update docs
+
+## v0.1.0 (2024-11-10)
+
+- Initial publish
--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule ServerSentEvents.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+
   @github_repo_url "https://github.com/benjreinhart/server_sent_events"
 
   @description "Lightweight, ultra-fast Server Sent Event parser"
@@ -8,7 +10,7 @@ defmodule ServerSentEvents.MixProject do
   def project do
     [
       app: :server_sent_events,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       name: "Server Sent Events",
@@ -16,7 +18,8 @@ defmodule ServerSentEvents.MixProject do
       source_url: @github_repo_url,
       homepage_url: @github_repo_url,
       package: package(),
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -30,7 +33,7 @@ defmodule ServerSentEvents.MixProject do
     @description
   end
 
-  def package do
+  defp package do
     [
       maintainers: ["Ben Reinhart"],
       licenses: ["MIT"],
@@ -43,6 +46,18 @@ defmodule ServerSentEvents.MixProject do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @github_repo_url,
+      source_ref: "v#{@version}",
+      extras: [
+        "README.md",
+        "CHANGELOG.md"
+      ]
     ]
   end
 end
